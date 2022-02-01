dd6fb23378139e93e8af03c730028bafa1a48597
Harreson_Lima <contatoharresonlima@gmail.com>
2022-02-01 16:05:11 -0300

    finished clok project

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setInterval(() => {
+        let Time = new Date();
 
-setInterval(()=>{
-    let Time = new Date();
+        days(Time.getDay());
 
-    document.getElementById("hours").innerHTML = Time.getHours();
-    document.getElementById("minutes").innerHTML = Time.getMinutes();
-    document.getElementById("seconds").innerHTML = Time.getSeconds();
-    document.getElementById("miliSeconds").innerHTML = Time.getMilliseconds();
-}, 1);
+        document.getElementById("hours").innerHTML =
+            (Time.getHours() < 10 ? "0" : "") + Time.getHours();
+        document.getElementById("minutes").innerHTML =
+            (Time.getMinutes() < 10 ? "0" : "") + Time.getMinutes();
+        document.getElementById("seconds").innerHTML =
+            (Time.getSeconds() < 10 ? "0" : "") + Time.getSeconds();
+    }, 1);
+});
+
+function days(date) {
+    let span = document.getElementById(`day-${date}`);
+    span.classList.add("contrast");
+}
